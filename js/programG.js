b522f9da52c0d678eb8f00f9b8a055fb3b68ca59
Katlego-ki <katlegobairvin@gmail.com>
2024-03-20 14:22:44 +0200

    On the grid, replaced div tag with img tag and added event listener 'click' to each img tag

--- js/programG.js
+++ js/programG.js
@@ -1,8 +1,16 @@
 const cardsGrid = document.querySelector('.cards-grid');
 
 for(let i = 0; i < 16; i++){
-    const newDiv = document.createElement("div");
-    newDiv.id = i;
-    cardsGrid.appendChild(newDiv);
+    const newImg = document.createElement("img");
+    newImg.id = i;
+    newImg.className= "cards";
+    newImg.addEventListener("click",flipImage);
+    cardsGrid.appendChild(newImg);
+
 }
 
+let icon = document.querySelector(".icons");
+
+function flipImage(){
+    alert('clicked left mouse key');
+}
