e310bae3535117ba167450b5b735cd2b9d973c1e
Katlego-ki <katlegobairvin@gmail.com>
2024-03-20 17:01:59 +0200

    added the flipImage function to each image

--- js/programG.js
+++ js/programG.js
@@ -4,13 +4,13 @@ for(let i = 0; i < 16; i++){
     const newImg = document.createElement("img");
     newImg.id = i;
     newImg.className= "cards";
-    newImg.addEventListener("click",flipImage);
+    newImg.onclick = function flipImage(){
+        newImg.src = "images/Card-Pictures/apple.webp";
+    };
     cardsGrid.appendChild(newImg);
 
 }
 
-let icon = document.querySelector(".icons");
+//let icon = document.getElementById("0");
 
-function flipImage(){
-    alert('clicked left mouse key');
-}
+
