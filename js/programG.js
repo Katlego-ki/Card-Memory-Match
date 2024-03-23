1b41a8983b8f95fc5b971dcfcfce576199512d4d
Katlego-ki <katlegobairvin@gmail.com>
2024-03-23 23:43:44 +0200

    step 1 of adding the compare function

--- js/programG.js
+++ js/programG.js
@@ -2,8 +2,12 @@ const cardsGrid = document.querySelector('.cards-grid');
 
 const cardPictures = {0: "images/Card-Pictures/apple.webp", 1:"images/Card-Pictures/dog.jpg",
  2:"images/Card-Pictures/boat.jpg", 3:"images/Card-Pictures/gazelle.webp", 4:"images/Card-Pictures/Hippo.jpg",
-  5:"images/Card-Pictures/monkey.jpg", 6:"images/Card-Pictures/rubix.webp", 7:"images/Card-Pictures/penguin.jpg"};
+ 5:"images/Card-Pictures/monkey.jpg", 6:"images/Card-Pictures/rubix.webp", 7:"images/Card-Pictures/penguin.jpg"};
 
+let imageCount = 0; //used for comparing 2 images
+let tempImages = new Array();
+
+//create img tags
 for(let i = 0; i < 16; i++){
     const newImg = document.createElement("img");
     newImg.id = i;
@@ -16,7 +20,37 @@ for(let i = 0; i < 16; i++){
         else{
             newImg.src = cardPictures[15-i];
         }
+
+        if(imageCount!=2){
+
+            console.log(imageCount + ": before");
+            imageCount++;
+            console.log(imageCount + ": after");
+
+            if(Number(newImg.id)<=7){
+                tempImages.push(cardPictures[Number(newImg.id)]);
+                console.log(tempImages);
+            }
+            else{
+                tempImages.push(cardPictures[Number(15 - newImg.id)]);
+                console.log(tempImages);
+            }
+            
+            if(imageCount===2){
+                compareImages(tempImages[0],tempImages[1]);
+                imageCount = 0;
+                tempImages = [];
+
+            }
+        }
         
     };
-    cardsGrid.appendChild(newImg);
+    cardsGrid.appendChild();
+}
+
+function compareImages(image1, image2){
+    if(image1 != image2){
+        image1.id = image2 = "../images/Unturned.png";
+    }
+    
 }
