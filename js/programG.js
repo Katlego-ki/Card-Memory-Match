db246cff29c219ca900d6efe494a5ad7ca1843cc
Katlego-ki <katlegobairvin@gmail.com>
2024-03-24 22:11:42 +0200

    added compare function

--- js/programG.js
+++ js/programG.js
@@ -6,6 +6,9 @@ const cardPictures = {0: "images/Card-Pictures/apple.webp", 1:"images/Card-Pictu
 
 let imageCount = 0; //used for comparing 2 images
 let tempImages = new Array();
+let image1,image2; //These images will be compared in pairs after being clicked.
+
+//Include a function to randomize pictures for every attempt!
 
 //create img tags
 for(let i = 0; i < 16; i++){
@@ -21,21 +24,17 @@ for(let i = 0; i < 16; i++){
             newImg.src = cardPictures[15-i];
         }
 
+        //when you're done try to make it more efficient and readable!
         if(imageCount!=2){
 
-            console.log(imageCount + ": before");
+            console.log(imageCount + ": before"); //debugging
             imageCount++;
-            console.log(imageCount + ": after");
-
-            if(Number(newImg.id)<=7){
-                tempImages.push(cardPictures[Number(newImg.id)]);
-                console.log(tempImages);
-            }
-            else{
-                tempImages.push(cardPictures[Number(15 - newImg.id)]);
-                console.log(tempImages);
-            }
+            console.log(imageCount + ": after"); //debugging
             
+            tempImages.push(newImg.id);
+            console.log(tempImages); //debugging
+            
+            //can we compare two selected pictures now?
             if(imageCount===2){
                 compareImages(tempImages[0],tempImages[1]);
                 imageCount = 0;
@@ -48,9 +47,15 @@ for(let i = 0; i < 16; i++){
     cardsGrid.appendChild(newImg);
 }
 
-function compareImages(image1, image2){
-    if(image1 != image2){
-        image1.src = image2.src = "../images/Unturned.png"; //fix, image1 is not an html element.
+function compareImages(id1, id2){
+
+    image1 = document.getElementById(id1);
+    image2 = document.getElementById(id2);
+
+    console.log(image1.src)// debugging
+
+    if(image1.src != image2.src){
+        image1.src = image2.src = "images/Card-Pictures/Unturned.png"; //fix, image1 is not an html element.
     }
     
 }
